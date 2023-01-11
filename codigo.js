d59fb4a76853da5fcc6bfa4ecb13879816c9c5e6
Cristian <diazrcm@gmail.com>
2023-01-11 16:29:05 -0500

    Function to validate Special Characters

--- codigo.js
+++ codigo.js
@@ -9,10 +9,26 @@ const btnEncriptar = document.querySelector("#encriptar");
 const btnDesncriptar = document.querySelector("#desencriptar");
 const btnCopiar = document.querySelector("#copiar");
 
+/*Función para validar carácteres con acento*/
+
+function validarMensaje (){
+    
+    let mensaje = String(entradaMensaje.value);
+    for (let i = 0; i < mensaje.length; i++){
+        
+        if(mensaje[i] === "á" || mensaje[i] === "é" || mensaje[i] === "í" || mensaje[i] === "ó" || mensaje[i] === "ú"){
+            
+            alert("Por favor no ingrese cáracteres especiales");
+            break;   
+        }
+    }    
+}
+
 /*Creacion de las funciones de encriptado, desencriptado y copiado*/
 
 function encriptarTexto(){
 /*Tomando la informacion del input y convirtiendola a string y reemplazando valores con el método replace*/ 
+    validarMensaje();
     let mensajeEncriptado = String(entradaMensaje.value).replace(/e/g,"enter")
     .replace(/i/g,"imes")
     .replace(/o/g,"ober")
@@ -24,6 +40,7 @@ function encriptarTexto(){
 }
 function desencriptarTexto(){
 /*Tomando la informacion del input y convirtiendola a string y reemplazando valores con el método replace*/ 
+    validarMensaje();
     let mensajeDesencriptado = String(entradaMensaje.value).replace(/enter/g,"e")
     .replace(/imes/g,"i")
     .replace(/ober/g,"o")
